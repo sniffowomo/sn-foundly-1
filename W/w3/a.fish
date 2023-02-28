1174f20b28357d314e453ab26f49591e8d236a5b
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-02-28 18:24:25 +0000

    🚆:w1

---
+++ W/w3/a.fish
@@ -0,0 +1,35 @@
+#!/bin/fish
+
+#******************************************DRINKHERPISSS**************************************************************#
+
+### APIS #### 
+set SETH CGWM4MY54A4NG7TZR3I3JY6F9JKHQD9F7Y
+set SMUM RVRYZNU5T1RJ56F5GKRKPDIYWYB8H59UNX
+set SBSC MEFZ8GWT9XJA5H7DWCAH1CJW92X92ZN4Q3 
+
+### RPC ALCHEMY - Alchemy RPCS's### 
+set SALETH https://eth-mainnet.g.alchemy.com/v2/RxbNgMVam_WwWaDpBw-itK6JP99WXGBS
+set SALGOE https://eth-goerli.g.alchemy.com/v2/J7mSlfOMfkB8Gi0l0Lyg3WV2N3tzBMg8
+set SALMUM https://polygon-mumbai.g.alchemy.com/v2/i9eChJfOCWZI7-RnrNGsMkEC6Xx9wix5
+set SALPOL https://polygon-mainnet.g.alchemy.com/v2/KSf3r9qxeTccxe4NRKVnXz24TJm8idOv
+set SALARBT https://arb-goerli.g.alchemy.com/v2/rQwjq_QQGzE_vJPOed9cqm2b-_nAAbKL
+
+### MORALIS ### 
+set SMORA sEhNeAz0NBm9RKJNFdJrC2VqtnKXc42VS7gcl7rcCmGvklghnlJue6vFd3cHfjlV
+
+### INFURA ### 
+set SIETH https://mainnet.infura.io/v3/cd03f6b1d3ad4c65ae3ee0cc42640cf7 
+set SIGOE https://goerli.infura.io/v3/cd03f6b1d3ad4c65ae3ee0cc42640cf7
+set SISEP https://sepolia.infura.io/v3/cd03f6b1d3ad4c65ae3ee0cc42640cf7
+set SISTRKM https://starknet-mainnet.infura.io/v3/cd03f6b1d3ad4c65ae3ee0cc42640cf7
+set SISTRKG https://starknet-goerli.infura.io/v3/cd03f6b1d3ad4c65ae3ee0cc42640cf7
+set SISTRKG2 https://starknet-goerli2.infura.io/v3/cd03f6b1d3ad4c65ae3ee0cc42640cf7
+set SIFILEC https://2L0NbnPxgjMsX5pJRly60ijIQEz:708875078da08c06b5560790f83a916e@filecoin.infura.io
+
+### QuickNODE BSC Testnet ### 
+set SQBSCT https://burned-dark-wave.bsc-testnet.discover.quiknode.pro/ba74335e452f9a1616f90976d70a65a482bd1736/
+
+### ANKR RPC https://www.ankr.com/rpc/ ### 
+### Chainlist RPCS - https://chainlist.org/ ### 
+
+#*****************************************SMELLHERFARTS******************************************************************#
